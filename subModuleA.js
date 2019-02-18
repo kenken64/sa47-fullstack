164531febfe0eef8681f3e732bc23274a365a0ba
Kenneth Phang <bunnyppl@gmail.com>
2019-02-18 16:09:56 +0800

    module exports

---
+++ subModuleA.js
@@ -0,0 +1,3 @@
+console.log("this is submodule A");
+y = 1 + 1;
+console.log(y);
---
+++ subModuleB.js
@@ -0,0 +1,11 @@
+exports.sayHelloInEnglish = function(){
+    return 'B - Hello';
+};
+
+exports.sayHelloInMalay = function(){
+    return 'B- Apa Khabar';
+};
+
+exports.sayHelloInChinese = function(){
+    return 'B - Ni Hao';
+};
